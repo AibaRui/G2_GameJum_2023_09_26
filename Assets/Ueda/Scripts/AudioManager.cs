e8ff9f1950779c42c734b0ba61b8217f8f19eb06
Uhiroya <72613038+Uhiroya@users.noreply.github.com>
2023-09-26 15:21:35 +0900

    [Update] AudioManager列挙型の変更

--- Assets/Ueda/Scripts/AudioManager.cs
+++ Assets/Ueda/Scripts/AudioManager.cs
@@ -6,25 +6,27 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
-    public enum InGameSoundType
+    //SEの割り当て
+    public enum SEType
     {
-        Fire = 0,
-        reload = 1,
-        explotion = 2,
-        reflectBullet = 3,
-        StartArea = 4,
-        SceneChange = 5,
-        Start = 6,
-        Sucseece = 7 ,
-        Fail = 8 ,
-        Fall = 9 ,
-        Landing = 10,
+        Acceleration = 0,    //加速
+        Deceleration = 1,    //減速
+        Clash = 2,        //衝突
+        Destruction = 3,    //破壊
+        BoostPanel = 4,        //ブーストパネル
+        LowEngine = 5,        //低速時エンジン音
+        HighEngine = 6,        //高速時エンジン音
+        Idle = 7,        //エンジンアイドル時
+        Start = 8,        //rece 開始時
+        Goal = 9,        //Goal時
     }
-    public enum BGMSceneType
+    //BGMの割り当て
+    public enum BGMType
     {
-        Title = 0, //
-        InGame = 1, //
-        Result = 2, //
+        Title = 0,        //タイトル画面で
+        LowSpeedBGM = 1,    //スピードが遅いときのBGM
+        HighSpeedBGM = 2,    //スピードが速いときのBGM
+        Result = 3,        //リザルト画面で
     }
     public AudioSource _audioSESource;
     public AudioSource _audioBGMSource;
@@ -42,11 +44,11 @@ public class AudioManager : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-    public void PlaySE(InGameSoundType soundIndex)
+    public void PlaySE(SEType soundIndex)
         => _audioSESource.PlayOneShot(_audioSEClips[(int)soundIndex]);
     public void PlaySE(int soundIndex)
         => _audioSESource.PlayOneShot(_audioSEClips[soundIndex]);
-    public void PlayBGM(BGMSceneType soundIndex)
+    public void PlayBGM(BGMType soundIndex)
     {
         _audioBGMSource.clip = _audioBGMClips[(int)soundIndex];
         _audioBGMSource.Play();
