99b73116f447400fe876ea9c895b4f1301a8763e
Aiba_0325 <bantan.ab325@gmail.com>
2023-09-26 13:57:21 +0900

    [Add] 移動処理の追加

---
+++ Assets/Aiba/Scritps/PlayerMove.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerMove
+{
+    [Header("à⁄ìÆç≈çÇë¨ìx")]
+    [SerializeField] private float _maxSpeed = 3;
+
+    [Header("â¡ë¨ìx")]
+    [SerializeField] private float _addSpeed;
+
+    [Header("å¥ë•ìx")]
+    [SerializeField] private float _slowSpeed;
+
+    private float _setSpeed = 0;
+
+    private float _beforInput = 0;
+
+    private bool _isChangeSpeed = false;
+
+    private PlayerControl _playerControl;
+    public void Init(PlayerControl playerControl)
+    {
+        _playerControl = playerControl;
+    }
+
+    public void SetSpeed()
+    {
+
+    }
+
+    public void Move(float h)
+    {
+        if (h != 0)
+        {
+            _playerControl.Rigidbody.AddForce(Vector3.right*h*_addSpeed);
+
+            if(_playerControl.Rigidbody.velocity.x> _maxSpeed)
+            {
+                _playerControl.Rigidbody.velocity = new Vector3(_maxSpeed, 0, 0);
+            }
+            else if(_playerControl.Rigidbody.velocity.x<-_maxSpeed)
+            {
+                _playerControl.Rigidbody.velocity = new Vector3(-_maxSpeed, 0, 0);
+            }
+        }
+        else
+        {
+            _setSpeed = 0;
+            if (_playerControl.Rigidbody.velocity.x == 0) return;
+
+                if (_playerControl.Rigidbody.velocity.x > 0)
+            {
+                _playerControl.Rigidbody.AddForce(Vector3.left * _slowSpeed);
+
+                if (_playerControl.Rigidbody.velocity.x < 0)
+                {
+                    _playerControl.Rigidbody.velocity = Vector3.zero;
+                }
+
+            }
+            else if (_playerControl.Rigidbody.velocity.x < 0)
+            {
+                _playerControl.Rigidbody.AddForce(Vector3.right * _slowSpeed);
+
+                if (_playerControl.Rigidbody.velocity.x > 0)
+                {
+                    _playerControl.Rigidbody.velocity = Vector3.zero;
+                }
+            }
+        }   //å∏ë¨èàóù
+
+        _beforInput = h;
+    }
+
+}
