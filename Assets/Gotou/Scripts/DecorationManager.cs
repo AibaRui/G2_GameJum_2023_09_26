d110d9828f4a0ce616ccd406276d8f638cdac377
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-09-27 12:00:01 +0900

    [add]デコレーション（木）の追加 [update]エフェクトの再生終了時に削除されるよう更新

---
+++ Assets/Gotou/Scripts/DecorationManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DecorationManager : MonoBehaviour, IDamageble
+{
+    [SerializeField] int _score = 0;
+    [SerializeField] ParticleSystem _breakEffect = null;
+    [SerializeField] float _waitTime = 0;
+    [SerializeField] SpriteRenderer _spriteRenderer = null;
+    [SerializeField] Transform _spriteTransform = null;
+
+    public void Hit()
+    {
+        // NowGear‚ª5‚ÌŽž‚Ì‚Ý”j‰ó‚Å‚«‚é
+        if (GameManager.Instance.NowGear == GameManager.Gear.Gear5)
+        {
+            GameManager.Instance.AddScore(_score);
+            _spriteRenderer.color = new Color(0, 0, 0, 0);
+            ParticleSystem particle = Instantiate(_breakEffect, _spriteTransform.transform.position, _spriteTransform.transform.rotation);
+            particle.Play();
+            Destroy(this.gameObject, _waitTime);
+        }
+    }
+}
